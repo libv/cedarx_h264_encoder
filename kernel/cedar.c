d9a50c62baa5a8b051a822f30fdab0515c5d1768
Luc Verhaegen <libv@skynet.be>
2019-09-06 21:45:00 +0200

    kernel: add /dev/ node
    Signed-off-by: Luc Verhaegen <libv@skynet.be>

--- kernel/cedar.c
+++ kernel/cedar.c
@@ -4,11 +4,20 @@
 #include <linux/module.h>
 #include <linux/of_device.h>
 #include <linux/platform_device.h>
+#include <linux/fs.h>
+#include <linux/cdev.h>
 
 #define MODULE_NAME "sunxi_cedar"
 
 struct sunxi_cedar {
 	struct device *dev;
+
+	/* for our /dev/ node */
+	struct cdev cdev;
+	struct class *class;
+	struct device *slashdev;
+	dev_t majorminor;
+
 	bool powered;
 };
 
@@ -62,9 +71,107 @@ static const struct dev_pm_ops cedar_pm_ops = {
 	SET_RUNTIME_PM_OPS(cedar_suspend, cedar_resume, NULL)
 };
 
+
+static int
+cedar_slashdev_open(struct inode *inode, struct file *filp)
+{
+	struct sunxi_cedar *cedar =
+		container_of(inode->i_cdev, struct sunxi_cedar, cdev);
+
+	dev_info(cedar->dev, "%s();\n", __func__);
+
+	filp->private_data = cedar;
+
+	return 0;
+}
+
+static int
+cedar_slashdev_release(struct inode *inode, struct file *filp)
+{
+	struct sunxi_cedar *cedar =
+		container_of(inode->i_cdev, struct sunxi_cedar, cdev);
+
+	dev_info(cedar->dev, "%s();\n", __func__);
+
+	filp->private_data = cedar;
+
+	return 0;
+}
+
+static long
+cedar_slashdev_ioctl(struct file *filp, unsigned int cmd, unsigned long arg)
+{
+	struct sunxi_cedar *cedar = filp->private_data;
+
+	dev_info(cedar->dev, "%s();\n", __func__);
+
+	return 0;
+}
+
+static int
+cedar_slashdev_mmap(struct file *filp, struct vm_area_struct *vma)
+{
+	struct sunxi_cedar *cedar = filp->private_data;
+
+	dev_info(cedar->dev, "%s();\n", __func__);
+
+	return 0;
+}
+
+static const struct file_operations cedar_slashdev_fops = {
+	.owner = THIS_MODULE,
+	.open = cedar_slashdev_open,
+	.release = cedar_slashdev_release,
+	.llseek = no_llseek,
+	.unlocked_ioctl = cedar_slashdev_ioctl,
+	.mmap = cedar_slashdev_mmap,
+};
+
 static int cedar_slashdev_initialize(struct sunxi_cedar *cedar)
 {
+	int ret;
+
 	dev_info(cedar->dev, "%s();\n", __func__);
+
+	ret = alloc_chrdev_region(&cedar->majorminor, 0, 1, "cedar_dev");
+	if (ret) {
+		dev_err(cedar->dev, "%s(): alloc_chrdev_region() failed: %d\n",
+			__func__, ret);
+		return ret;
+	}
+
+	cdev_init(&cedar->cdev, &cedar_slashdev_fops);
+	cedar->cdev.owner = THIS_MODULE;
+
+	ret = cdev_add(&cedar->cdev, cedar->majorminor, 1);
+	if (ret) {
+		dev_err(cedar->dev, "%s(): cdev_add() failed: %d\n",
+			__func__, ret);
+		unregister_chrdev_region(cedar->majorminor, 1);
+		return ret;
+	}
+
+	cedar->class = class_create(THIS_MODULE, "cedar_dev");
+	if (IS_ERR(cedar->class)) {
+		dev_err(cedar->dev, "%s(): class_create() failed: %ld\n",
+			__func__, PTR_ERR(cedar->class));
+		cdev_del(&cedar->cdev);
+		unregister_chrdev_region(cedar->majorminor, 1);
+		return ret;
+
+	}
+
+	cedar->slashdev = device_create(cedar->class, NULL, cedar->majorminor,
+					NULL, "cedar_dev");
+	if (!cedar->slashdev) {
+		dev_err(cedar->dev, "%s(): device_create() failed: %d\n",
+			__func__, ret);
+		class_destroy(cedar->class);
+		cedar->class = NULL;
+		cdev_del(&cedar->cdev);
+		unregister_chrdev_region(cedar->majorminor, 1);
+		return ret;
+	}
 
 	return 0;
 }
@@ -72,6 +179,13 @@ static int cedar_slashdev_initialize(struct sunxi_cedar *cedar)
 static int cedar_slashdev_cleanup(struct sunxi_cedar *cedar)
 {
 	dev_info(cedar->dev, "%s();\n", __func__);
+
+	device_destroy(cedar->class, cedar->majorminor);
+	cedar->slashdev = NULL;
+	class_destroy(cedar->class);
+	cedar->class = NULL;
+	cdev_del(&cedar->cdev);
+	unregister_chrdev_region(cedar->majorminor, 1);
 
 	return 0;
 }
