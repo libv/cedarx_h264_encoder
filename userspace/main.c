19b35f753532c8ef2f2f083558ed0ae43067cd2a
Luc Verhaegen <libv@skynet.be>
2019-11-11 17:00:22 +0100

    user: move ve.c to main.c
    Signed-off-by: Luc Verhaegen <libv@skynet.be>

--- userspace/main.c
+++ userspace/main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2015 Jens Kuske <jenskuske@gmail.com>
+ * Copyright (c) 2013-2015 Jens Kuske <jenskuske@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -29,13 +29,147 @@
 #include <sys/stat.h>
 #include <errno.h>
 #include <stdbool.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 
-#include "h264enc.h"
-#include "ve.h"
+#include "cedar_ioctl.h"
+
+#define DEVICE "/dev/cedar_dev"
 
 #define IS_ALIGNED(x, a) (((x) & ((typeof(x))(a) - 1)) == 0)
 
 static uint32_t frame_count;
+
+static void *input_buffer_virtual;
+static int input_buffer_size;
+
+static void *bytestream_virtual;
+static int bytestream_size;
+
+static int cedar_fd = -1;
+
+struct h264enc_params {
+	unsigned int width;
+	unsigned int height;
+
+	unsigned int src_width;
+	unsigned int src_height;
+	enum color_format { H264_FMT_NV12 = 0, H264_FMT_NV16 = 1 } src_format;
+
+	unsigned int profile_idc, level_idc;
+
+	enum { H264_EC_CAVLC = 0, H264_EC_CABAC = 1 } entropy_coding_mode;
+
+	unsigned int qp;
+
+	unsigned int keyframe_interval;
+};
+
+int ve_open(void)
+{
+	cedar_fd = open(DEVICE, O_RDWR);
+	if (cedar_fd == -1) {
+		fprintf(stderr, "%s(): failed to open %s: %s\n",
+			__func__, DEVICE, strerror(errno));
+		return -1;
+	}
+
+	return 0;
+}
+
+void
+ve_close(void)
+{
+	close(cedar_fd);
+	cedar_fd = -1;
+}
+
+int
+ve_encode(void)
+{
+	int ret;
+
+	ret = ioctl(cedar_fd, CEDAR_IOCTL_ENCODE, NULL);
+	if (ret < 0)
+		fprintf(stderr, "%s(): CEDAR_IOCTL_ENCODE failed: %s\n",
+			__func__, strerror(errno));
+
+	return ret;
+}
+
+int ve_config(struct h264enc_params *params)
+{
+	struct cedar_ioctl_config config = { 0 };
+	int ret;
+
+	config.src_width = params->src_width;
+	config.src_height = params->src_height;
+
+	if (params->src_format == H264_FMT_NV16)
+		config.src_format = CEDAR_IOCTL_CONFIG_FORMAT_NV16;
+	else
+		config.src_format = CEDAR_IOCTL_CONFIG_FORMAT_NV12;
+
+	config.dst_width = params->width;
+	config.dst_height = params->height;
+
+	config.profile = params->profile_idc;
+	config.level = params->level_idc;
+	config.qp = params->qp;
+	config.keyframe_interval = params->keyframe_interval;
+
+	if (params->entropy_coding_mode == H264_EC_CABAC)
+		config.entropy_coding_mode = CEDAR_IOCTL_ENTROPY_CODING_CABAC;
+	else
+		config.entropy_coding_mode = CEDAR_IOCTL_ENTROPY_CODING_CAVLC;
+
+	ret = ioctl(cedar_fd, CEDAR_IOCTL_CONFIG, &config);
+	if (ret) {
+		fprintf(stderr, "%s(): CEDAR_IOCTL_CONFIG failed: %s\n",
+			__func__, strerror(errno));
+		return ret;
+	}
+
+	input_buffer_size = config.input_size;
+	input_buffer_virtual =
+		mmap(NULL, input_buffer_size, PROT_READ | PROT_WRITE,
+		     MAP_SHARED, cedar_fd, config.input_dma_addr);
+	if (input_buffer_virtual == MAP_FAILED) {
+		fprintf(stderr, "%s(): failed to mmap input buffer: %s.\n",
+			__func__, strerror(errno));
+		return -ENOMEM;
+	}
+
+	printf("Input: %dbytes at 0x%08X -> %p\n", input_buffer_size,
+	       config.input_dma_addr, input_buffer_virtual);
+
+	bytestream_size = config.bytestream_size;
+	bytestream_virtual =
+		mmap(NULL, bytestream_size, PROT_READ | PROT_WRITE,
+		     MAP_SHARED, cedar_fd, config.bytestream_dma_addr);
+	if (bytestream_virtual == MAP_FAILED) {
+		fprintf(stderr, "%s(): failed to mmap input buffer: %s.\n",
+			__func__, strerror(errno));
+		return -ENOMEM;
+	}
+
+	printf("Bytestream: %dbytes at 0x%08X -> %p\n", bytestream_size,
+	       config.bytestream_dma_addr, bytestream_virtual);
+
+	return 0;
+}
+
+void *
+ve_input_buffer_virtual_get(void)
+{
+	return input_buffer_virtual;
+}
+
+void *
+ve_bytestream_virtual_get(void)
+{
+	return bytestream_virtual;
+}
 
 static int
 h264enc_new(struct h264enc_params *params)
