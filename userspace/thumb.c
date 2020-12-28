da403c5626ada2112c64d01531235044f031bd59
Luc Verhaegen <libv@skynet.be>
2020-12-28 15:27:04 +0100

    thumb: add config and mapping
    Signed-off-by: Luc Verhaegen <libv@skynet.be>

--- userspace/thumb.c
+++ userspace/thumb.c
@@ -22,18 +22,96 @@
 #include <errno.h>
 #include <inttypes.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
 
 #include "cedar_ioctl.h"
 
+#define ALIGN(x, a) (((x) + ((typeof(x))(a) - 1)) & ~((typeof(x))(a) - 1))
+
+#define DEFAULT_WIDTH 64
+#define DEFAULT_HEIGHT 32
+
 static int cedar_fd = -1;
+
+static int input_width;
+static int input_height;
+static int input_pitch;
+static uint8_t *input_luma;
+static size_t input_luma_size;
+static uint8_t *input_chroma;
+static size_t input_chroma_size;
+
+static int
+cedar_config(int width, int height)
+{
+	struct cedar_ioctl_config config[1] = {{
+		.src_width = width,
+		.src_height = height,
+		.src_format = CEDAR_IOCTL_CONFIG_FORMAT_NV12,
+		.dst_width = ALIGN(width, 16),
+		.dst_height = ALIGN(height, 16),
+		.profile = 77,
+		.level = 41,
+		.qp = 24,
+		.keyframe_interval = 25,
+		.entropy_coding_mode = CEDAR_IOCTL_ENTROPY_CODING_CABAC,
+	}};
+	int ret;
+
+	ret = ioctl(cedar_fd, CEDAR_IOCTL_CONFIG, config);
+	if (ret) {
+		fprintf(stderr, "Error:%s():ioctl(CONFIG): %s.\n",
+			__func__, strerror(errno));
+		return errno;
+	}
+
+	input_width = width;
+	input_height = height;
+	input_pitch = width;
+
+	input_luma_size = config->input_luma_size;
+	input_luma = mmap(NULL, input_luma_size, PROT_READ | PROT_WRITE,
+			  MAP_SHARED, cedar_fd, config->input_luma_dma_addr);
+	if (input_luma == MAP_FAILED) {
+		fprintf(stderr, "Error: %s(): mmap(luma): %s.\n",
+			__func__, strerror(errno));
+		return errno;
+	}
+	printf("Input Y: 0x%08X -> %p (%tdbytes).\n",
+	       config->input_luma_dma_addr, input_luma, input_luma_size);
+
+	input_chroma_size = config->input_chroma_size;
+	input_chroma = mmap(NULL, input_chroma_size, PROT_READ | PROT_WRITE,
+			  MAP_SHARED, cedar_fd, config->input_chroma_dma_addr);
+	if (input_chroma == MAP_FAILED) {
+		fprintf(stderr, "Error: %s(): mmap(chroma): %s.\n",
+			__func__, strerror(errno));
+		return errno;
+	}
+	printf("Input C: 0x%08X -> %p (%tdbytes).\n",
+	       config->input_chroma_dma_addr, input_chroma, input_chroma_size);
+
+	return 0;
+}
 
 int main(int argc, char *argv[])
 {
+	int ret;
+
 	cedar_fd = open(CEDAR_DEVICE_PATH, O_RDWR);
 	if (cedar_fd == -1) {
 		fprintf(stderr, "Error: %s():open(%s): %s\n",
 			__func__, CEDAR_DEVICE_PATH, strerror(errno));
 		return errno;
+	}
+
+	ret = cedar_config(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+	if (ret) {
+		fprintf(stderr, "Error: %s(): cedar_config(): %s.\n",
+			__func__, strerror(-ret));
+		return ret;
 	}
 
 	return 0;
